a0bb12ee2e7628cd3530642b8f894884f2a06830
TheKany <skarnjs06@naver.com>
2023-01-27 16:51:17 +0900

    🚧 cors,helmet / dayjs / bcrypt, jwt 연습

--- express-practice/src/index.js
+++ express-practice/src/index.js
@@ -1,6 +1,32 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dayjs from "dayjs";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
+
+// 미들웨어 등록
+app.use(
+  cors({
+    origin: "*", // 도메인, * 표시의 경우 전체
+  })
+);
+app.use(helmet());
+
+const today = new Date();
+const todayToDayjs = dayjs(today).format("YYYY-MM-DD");
+console.log(`원래 날짜 표기: ${today}`);
+console.log(`dayjs 날짜 표기: ${todayToDayjs}`);
+
+// bcrypt와 jwt
+const password = "1234";
+const hashedPW = bcrypt.hashSync(password, 10);
+console.log({ hashedPW });
+
+const token = jwt.sign("1234", "시크릿키");
+console.log({ token });
 
 app.get("/", (req, res) => {
   res.send("Express");
